485e50d173966a1265ae969b5382975dde0c43a4
Reece Thandi <reecethandi11@gmail.com>
2025-04-05 20:14:25 +0100

    added db schema from supabase

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
-import {
-  AuthResponse,
-  createClient,
-  SupabaseClient,
-} from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment.development';
 import { from, map, Observable } from 'rxjs';
 import { TodoInterface } from '../types/todo.interace';
+import { Database } from '../types/database.types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private supabase: SupabaseClient = createClient(
+  private supabase: SupabaseClient = createClient<Database>(
     environment.supabaseUrl,
     environment.supabaseKey
   );
